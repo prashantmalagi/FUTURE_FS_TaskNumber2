78fb2b87d6d4a7e00ec35a8537daf53c185e09e3
Prashant Malagi <prashantmalagi60@gmail.com>
2026-02-19 21:14:20 +0530

    fix render port

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.get("/", (req, res) => {
     res.send("Mini CRM Server Running");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
