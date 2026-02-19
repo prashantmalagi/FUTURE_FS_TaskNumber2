b14858e4215189cd3957ed449d05f5001d86cf1f
Prashant Malagi <prashantmalagi60@gmail.com>
2026-02-19 23:40:41 +0530

    fix admin creation

--- server.js
+++ server.js
@@ -7,16 +7,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://prashantmalagi60_db_user:admin123@cluster0.oqgqzrt.mongodb.net/minicrm")
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+const Admin = require("./models/Admin");
+
+// ✅ Connect MongoDB properly
+mongoose
+  .connect("mongodb+srv://prashantmalagi60_db_user:admin123@cluster0.oqgqzrt.mongodb.net/minicrm")
+  .then(async () => {
+    console.log("MongoDB Connected");
+
+    // ✅ Create default admin AFTER DB connected
+    const exists = await Admin.findOne({ username: "admin" });
+
+    if (!exists) {
+      await Admin.create({
+        username: "admin",
+        password: "admin123",
+      });
+      console.log("Default admin created");
+    }
+  })
+  .catch((err) => console.log(err));
 
 app.use("/api/leads", require("./routes/leadRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 
-
 app.get("/", (req, res) => {
-    res.send("Mini CRM Server Running");
+  res.send("Mini CRM Server Running");
 });
 
 const PORT = process.env.PORT || 5000;
@@ -24,4 +40,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
-
